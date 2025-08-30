e11398bc49cac371be1862c7734fedd1d02450b0
ronak bendwal <ronakbendwal7880@gmail.com>
2025-08-31 01:58:16 +0530

    write the blog controller

---
+++ src/Controller/blog.controller.js
@@ -0,0 +1,102 @@
+import { BLOG } from "../Models/blog.Model"
+import { USER } from "../Models/user.models"
+import {apiError} from "../Utils/apiError"
+import {apiResponse} from "../Utils/apiResponse"
+import {asyncHandle} from "../Utils/asyncHand"
+
+const createBlog=asyncHandle(async(req,res)=>{
+  //get the data from the body
+  //check field are not empty
+  //check blog is exist or not 
+  //create new blog
+
+  const user=await USER.findById(req.user?._id)
+  if(!user){
+    throw new apiError(401,"user not found")
+  }
+  const {title}=req.body
+  if(title?.trim()===""){
+    throw new apiError(401,"Title required")
+  }
+
+  const have=await BLOG.findOne({title})
+
+  if(have){
+    throw new apiError(401,"blog already exist")
+  }
+
+  const blogObject=await BLOG.create({
+    title,
+    owner:user?._id
+  })
+
+  const blogObjectReferance=await BLOG.findById(blogObject?._id)
+  if(!blogObjectReferance){
+    throw new apiError(500,"Issue While Creating Blog")
+  }
+
+  return res.status(201)
+  .json(
+    new apiResponse(
+      201,
+      blogObjectReferance,
+      "Blog Sucessfully Created"
+    )
+  )
+})
+
+const deleteBlog=asyncHandle(async(req,res)=>{
+  //get the blog info from params
+  const {title}=req.params
+  if(!title?.trim()===""){
+    throw new apiError(401,"Blog Not Found")
+  }
+
+  await BLOG.findOneAndDelete(title)
+
+  return res.status(201)
+  .json(new apiResponse(201,
+    {},
+    "sucessfully delete the blog"
+  ))
+
+})
+
+const updateBlog=asyncHandle(async(req,res)=>{
+  //get the new data from user body
+  const {title}=req.body
+  if(!title?.trim()==""){
+    throw new apiError(401,"fields are required")
+  }
+  const updated=await BLOG.findOneAndUpdate(title,
+    {
+      $set:{
+        title
+      }
+    },{new:true}
+  )
+  if(!updateBlog){
+    throw new apiError(500,"Blog not updated")
+  }
+
+  return res.status(201)
+  .json(new apiResponse(200,updateBlog,"Sucessfully Updated"))
+})
+
+const getCurrentBlog=asyncHandle(async(req,res)=>{
+  const user=await USER.findById(req.user?._id)
+  if(!user){
+    throw new apiError(401,"User Not Found")
+  }
+  const blogData=await BLOG.findById(user?._id)
+
+  return res.status(200)
+  .json(new apiResponse(200,blogData,"sucessfully get the blog"))
+})
+
+export {
+  createBlog,
+  deleteBlog,
+  updateBlog,
+  getCurrentBlog
+}
